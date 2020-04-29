b39b28bf011cab9dc2c229afad12b59043b322e7
Emilie Ørtoft Høllede Pedersen <ehp@nykredit.dk>
2020-04-29 08:23:13 +0200

    added cards and styling

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 
 import Card from './components/card/card';
 import calculator from './images/icon-calculator.svg';
+import teamBuilder from './images/icon-team-builder.svg';
+import karma from './images/icon-karma.svg';
+import supervisor from './images/icon-supervisor.svg';
 
 function App() {
   return (
     <div className="App">
-        <Card title="This is a title" image={calculator}>
-          This is a paragraph
+        <Card title="Supervisor" image={supervisor} color="cyan">
+          Monitors activity to identify project roadblocks
+        </Card>
+        <Card title="Team Builder" image={teamBuilder} color="red">
+          Scans our talent network to create the optimal team for your projects
+        </Card>
+        <Card title="Karma" image={karma} color="yellow">
+          Regularly evaluates our talent to ensure quality
+        </Card>
+        <Card title="Calculator" image={calculator} color="blue">
+          Uses data from past projects to provide better delivery estimates
         </Card>
     </div>
   );
--- src/components/card/card.js
+++ src/components/card/card.js
@@ -3,7 +3,7 @@ import './card.css';
 
 const Card = ( props ) => {
     return (
-        <div className="card">
+        <div className="card" id={props.color}>
             <h1 className="card-title">{props.title}</h1>
             <p className="card-paragraph">{props.children}</p>
             <img className="card-image" src={props.image} alt={props.alt}/>
