eff4aab8f19e593ef8d2275ebe9f296f27c5dadb
Emilie Ørtoft Høllede Pedersen <ehp@nykredit.dk>
2020-04-30 10:16:07 +0200

    style and favicon

--- src/App.js
+++ src/App.js
@@ -10,18 +10,31 @@ import supervisor from './images/icon-supervisor.svg';
 function App() {
   return (
     <div className="App">
-        <Card title="Supervisor" image={supervisor} color="cyan">
-          Monitors activity to identify project roadblocks
-        </Card>
-        <Card title="Team Builder" image={teamBuilder} color="red">
-          Scans our talent network to create the optimal team for your projects
-        </Card>
-        <Card title="Karma" image={karma} color="yellow">
-          Regularly evaluates our talent to ensure quality
-        </Card>
-        <Card title="Calculator" image={calculator} color="blue">
-          Uses data from past projects to provide better delivery estimates
-        </Card>
+      <div className="header">
+        <p id="header-title">Reliabe, efficient delivery</p>
+        <p id="header-title-bold">Powered by Technology</p>
+        <p id="header-subtitle">Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+      </div>
+      <div className="card-container">
+        <div className="column">
+          <Card title="Supervisor" image={supervisor} color="cyan">
+            Monitors activity to identify project roadblocks
+          </Card>
+        </div>
+        <div className="column">
+          <Card title="Team Builder" image={teamBuilder} color="red">
+            Scans our talent network to create the optimal team for your projects
+          </Card>
+          <Card title="Karma" image={karma} color="yellow">
+            Regularly evaluates our talent to ensure quality
+          </Card>
+        </div>
+        <div className="column">
+          <Card title="Calculator" image={calculator} color="blue">
+            Uses data from past projects to provide better delivery estimates
+          </Card>
+        </div>
+      </div> 
     </div>
   );
 }
